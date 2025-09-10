dac7698e8b2b3d53ae1ea157dc13bf03f5c46286
BunnySasi <sasikiran2213@gmail.com>
2025-09-10 14:07:17 +0530

    commit

---
+++ src/test/java/org/playwright/basics/Basics.java
@@ -0,0 +1,37 @@
+package org.playwright.basics;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.testng.annotations.BeforeSuite;
+import org.testng.annotations.Test;
+
+import com.microsoft.playwright.Browser;
+import com.microsoft.playwright.Browser.NewContextOptions;
+import com.microsoft.playwright.BrowserContext;
+import com.microsoft.playwright.BrowserType.LaunchOptions;
+import com.microsoft.playwright.Page;
+import com.microsoft.playwright.Playwright;
+
+public class Basics {
+
+	public Playwright playwright;
+	public Browser browser;
+	public BrowserContext browserContext;
+	public Page page;
+	@BeforeSuite
+	public void launch() {
+		playwright = Playwright.create();
+		List<String>a = new ArrayList<>();
+		a.add("--start-maximized");
+		browser = playwright.chromium().launch(new LaunchOptions().setHeadless(false).setArgs(a));
+		browserContext = browser.newContext(new NewContextOptions().setViewportSize(null));
+		page = browserContext.newPage();
+		page.navigate("https://www.google.com");
+		page.pause();
+	}
+	@Test
+	public void testOne() throws Exception {
+		
+	}
+}
